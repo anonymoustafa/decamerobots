e985845926fa87a62259539ef26a6edb80ea9afa
anonymoustafa <anonymoustafa@outlook.com>
2022-02-11 01:30:05 +0330

    gitignore updated, so the robot tokens shall be safe. also, vim swap files won't be uploaded to github anymore.

---
+++ 001/app.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const TelegramBot = require('node-telegram-bot-api');
+const my_hello_world_robot_token = process.env.my_hello_world_robot_token;
+console.log('h');
+//
+
