e8259576794709e31678c1e3251d4425292bfc71
anonymoustafa <anonymoustafa@outlook.com>
2022-02-12 12:33:21 +0330

    Constructed my Telegram hello-bot :)

--- 001/app.js
+++ 001/app.js
@@ -1,6 +1,14 @@
-require('dotenv').config()
-const TelegramBot = require('node-telegram-bot-api');
-const my_hello_world_robot_token = process.env.my_hello_world_robot_token;
-console.log('h');
-//
+ require('dotenv').config();
+ const {Telegraf} = require('telegraf');
+ const bot = new Telegraf(process.env.my_hello_world_robot_token);
+ bot.start((ctx) => ctx.reply('Welcome'));
+ bot.help((ctx) => ctx.reply('Send me a sticker'));
+ bot.on('sticker', (ctx) => ctx.reply('👍'));
+ bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+ bot.launch();
+ 
+ //  Enable graceful stop
+ process.once('SIGINT', () => bot.stop('SIGINT'));
+ process.once('SIGTERM', () => bot.stop('SIGTERM'));
+ console.log(process.env.my_hello_world_robot_token);
 
